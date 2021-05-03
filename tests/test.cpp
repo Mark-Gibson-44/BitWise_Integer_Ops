d878b0f0d224fc2b8377c6c3fb668f33e54af75c
Mark-Gibson-44 <mjgibson44@outlook.com>
2021-05-03 13:14:55 +0100

    Adding tests to code whilst looking into setting up cpp20 concepts, issue seems to exist with bitwise or@

---
+++ tests/test.cpp
@@ -0,0 +1,25 @@
+#include <gtest/gtest.h>
+#include "../Operations.h"
+
+TEST(Addition, BasicAssertions) {
+  // Expect two strings not to be equal.
+  
+  // Expect equality.
+  EXPECT_EQ(ArithmeticLogicOps::add(4,5), 4 + 5);
+}
+
+TEST(Negation, BasicAssertions){
+
+  EXPECT_EQ(ArithmeticLogicOps::neg(80,43), 80 - 43);
+}
+
+TEST(BITWISE_And, BasicAssertions){
+
+  EXPECT_EQ(ArithmeticLogicOps::bitwise_and(32, 91), 32 & 91);
+}
+
+TEST(BITWISE_Or, BasicAssertions){     
+                                        
+  EXPECT_EQ(ArithmeticLogicOps::bitwise_or(32, 91), 32 | 91);
+} 
+
